0b6c9eaf0a96f88fadc6698e00c7189c9422afdd
Gianluca Benassi <benox1199@gmail.com>
2021-11-10 15:22:17 +0100

    layout structure + dice icon

--- js/script.js
+++ js/script.js
@@ -4,24 +4,27 @@ const mailList = ["gianluca@gmail.com", "boolean@gmail.com", "email@gmail.com", 
 const btnLogin = document.getElementById("login");
 
 btnLogin.addEventListener("click", function() {
-    const userEmail = document.getElementById("email").value;
-    const spanLoginResult = document.getElementById("login-result");
+    const userEmail = document.getElementById("email");
     let emailCheck = false;
     
-    if (userEmail == "") {
-        spanLoginResult.innerHTML = "Inserire una email!";
+    if (userEmail.value == "") {
+        alert("Inserire una email!");
     } else {
 
         for (let i = 0; i < mailList.length; i++) {
-            if (userEmail == mailList[i]) {
+            if (userEmail.value == mailList[i]) {
                 emailCheck = true;
             }
         }
 
         if (emailCheck) {
-            spanLoginResult.innerHTML = "Accesso eseguito!"
+            alert("Accesso eseguito!");
+
+            userEmail.disabled = true;
+            btnLogin.disabled = true;
+
         } else {
-            spanLoginResult.innerHTML = "Email non valida!"
+            alert("Email non valida!");
         }
 
     }
@@ -33,12 +36,18 @@ btnLogin.addEventListener("click", function() {
 const btnDice = document.getElementById("roll-dice");
 
 btnDice.addEventListener("click", function(){
+
     const spanGameResult = document.getElementById("game-result");
+    const iconNumber = ["one", "two", "three", "four", "five", "six"]
+
     const userNumber = Math.floor(Math.random() * 6) + 1;
     const computerNumber = Math.floor(Math.random() * 6) + 1;
 
-    document.getElementById("user-number").innerHTML += userNumber;
-    document.getElementById("computer-number").innerHTML += computerNumber;
+    const userDice = document.getElementById("user-dice");
+    const computerDice = document.getElementById("computer-dice");
+
+    userDice.innerHTML = `<i class="fas fa-dice-${iconNumber[userNumber - 1]}"></i>`
+    computerDice.innerHTML = `<i class="fas fa-dice-${iconNumber[computerNumber - 1]}"></i>`
 
     if (userNumber > computerNumber) {
         spanGameResult.innerHTML = "Hai vinto!";
